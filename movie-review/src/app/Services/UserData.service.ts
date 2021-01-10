fe13660606e25ea1563fced6021ff7903a0b903e
JustinM44 <justin.mechanye@outlook.com>
2021-01-10 14:09:39 -0600

    update multiple files

---
+++ movie-review/src/app/Services/UserData.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+private userName: string;
+private userId: number;
+private userRole: string;
+
+constructor() { }
+
+getUserName(): string{
+  return this.userName;
+}
+
+setUserName(name: string): void{
+  this.userName = name;
+}
+
+getUserId(): number{
+  return this.userId;
+}
+setUserId(userId: number):void{
+  this.userId = userId;
+}
+
+getUserRole(): string{
+  return this.userRole;
+}
+setUserRole(role: string){
+  this.userRole = role;
+}
+
+}
